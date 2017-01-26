c22bea5c424bfb7887e7ed2a8f02555645986479
stephengrider <ste.grider@gmail.com>
2017-01-26 10:21:50 -0800

    clear out schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,5 @@
-const graphql = require('graphql');
 const _ = require('lodash');
-const axios = require('axios');
+const graphql = require('graphql');
 const {
   GraphQLObjectType,
   GraphQLString,
@@ -10,66 +9,14 @@ const {
   GraphQLNonNull
 } = graphql;
 
-const UserType = new GraphQLObjectType({
-  name: 'User',
-  fields: () => ({
-    id: { type: GraphQLString },
-    email: { type: GraphQLString }
-  })
-});
-
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
-  fields: {
-    user: {
-      type: UserType,
-      args: { id: { type: GraphQLString } },
-      resolve(parentValue, args, context) {
-
-      }
-    }
-  }
+  fields: {}
 });
 
 const mutation = new GraphQLObjectType({
   name: 'Mutation',
-  fields: {
-    addUser: {
-      type: UserType,
-      args: {
-        firstName: { type: new GraphQLNonNull(GraphQLString) },
-        age: { type: new GraphQLNonNull(GraphQLInt) },
-        companyId: { type: GraphQLString }
-      },
-      resolve(parentValue, { firstName, age }) {
-        return axios.post('http://localhost:3000/users', { firstName, age })
-          .then(res => res.data);
-      }
-    },
-    deleteUser: {
-      type: UserType,
-      args: {
-        id: { type: new GraphQLNonNull(GraphQLString) }
-      },
-      resolve(parentValue, { id }) {
-        return axios.delete(`http://localhost:3000/users/${id}`)
-          .then(res => res.data);
-      }
-    },
-    editUser: {
-      type: UserType,
-      args: {
-        id: { type: new GraphQLNonNull(GraphQLString) },
-        firstName: { type: GraphQLString },
-        age: { type: GraphQLInt },
-        companyId: { type: GraphQLString }
-      },
-      resolve(parentType, args) {
-        return axios.patch(`http://localhost:3000/users/${args.id}`, args)
-          .then(res => res.data);
-      }
-    }
-  }
+  fields: {}
 });
 
 module.exports = new GraphQLSchema({
